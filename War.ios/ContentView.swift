e2ac8c8408e4d345e17704fe0f149e8b1dd361d4
Michael Cuellar <Michaeldev720@icloud.com>
2023-03-11 04:32:21 -0700

    Initial Commit

---
+++ War.ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  War.ios
+//
+//  Created by Michael Cuellar on 3/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War.ios/War_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_iosApp.swift
+//  War.ios
+//
+//  Created by Michael Cuellar on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct War_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
